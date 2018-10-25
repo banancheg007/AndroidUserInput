20553c64233f9abbf390157ae9d535ec76a6bd08
Ruslan <ruslansolodovnyk@gmail.com>
2018-10-25 15:37:08 +0300

    Done decrement and increment

--- app/src/main/java/com/example/banancheg/androiduserinput/MainActivity.java
+++ app/src/main/java/com/example/banancheg/androiduserinput/MainActivity.java
@@ -9,15 +9,28 @@ import java.text.NumberFormat;
 
 public class MainActivity extends AppCompatActivity {
 
+    int quantity = 0;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
 
+    public void increment(View view){
+        quantity = quantity + 1;
+        display(quantity);
+    }
+
+
+    public void decrement(View view){
+        quantity = quantity - 1;
+        display(quantity);
+    }
+
     public void submitOrder(View view){
-        display(2);
-        displayPrice(2 * 5);
+        display(quantity);
+        displayPrice(quantity * 5);
     }
 
     private void display(int number){
